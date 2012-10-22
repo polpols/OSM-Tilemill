aea5d330680c3f44e5561b0ae66cbfc08f078ab7
polpols <polpols@me.com>
2012-10-22 15:05:49 +0200

    some more scripts

---
+++ OSM/osm.sh
@@ -0,0 +1,1 @@
+osm2pgsql --create --database suisse --prefix suisse  --style /usr/local/share/osm2pgsql/default.style --hstore-column "swisstopo:" /Volumes/DIVERS/GH/OSM-Tilemill/OSM/switzerland.osm
