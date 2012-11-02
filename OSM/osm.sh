253d5c7b11b7ac4705830e1cb4497cc97aa69a11
polpols <polpols@me.com>
2012-11-02 09:47:20 +0100

    clean

--- OSM/osm.sh
+++
@@ -1,1 +0,0 @@
-osm2pgsql --create --database suisse --prefix suisse  --style /usr/local/share/osm2pgsql/default.style --hstore-column "swisstopo:" /Volumes/DIVERS/GH/OSM-Tilemill/OSM/switzerland.osm
--- OSM/osm2psq-suisse.sh
+++
@@ -1,1 +0,0 @@
-time osm2pgsql --create --database suisse  --hstore-all --style /usr/local/share/osm2pgsql/default.style --prefix suisse /Volumes/DIVERS/GH/OSM-Tilemill/OSM/switzerland.osm
